f4864a576a65cdbf5e8be59a7749de3c13c04bc3
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2021-12-23 21:00:29 +0300

    gsap animations

--- New/gsap.js
+++ New/gsap.js
@@ -2,13 +2,8 @@
 const timeline = gsap.timeline()
 timeline
 .from(
-    '.myimg',{opacity :0,rotation: 180, duration: 8, ease: 'bounce'}
+    '.myimg',{opacity :0,rotation: 180, duration: 8, ease: 'bounce',scale: 0.3}
 )
 .from(
     '.profile',{x: '-100%', opacity: 0, ease:'power 2', duration:3}
-)
-.from(
-    '.ordered-menu',
-        {opacity:0}
-    
 )
