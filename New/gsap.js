7f8c305f01f63f76d1325f19885b78639590a3af
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-01-13 09:47:09 +0300

    my adjustment commit

--- New/gsap.js
+++ New/gsap.js
@@ -2,7 +2,7 @@
 const timeline = gsap.timeline()
 timeline
 .from(
-    '.myimg',{opacity :0,rotation: 180, duration: 8, ease: 'bounce',scale: 0.3}
+    '.myimg',{opacity :0,rotation: 180, duration: 5, ease: 'bounce',scale: 0.3}
 )
 .from(
     '.profile',{x: '-100%', opacity: 0, ease:'power 2', duration:3}
